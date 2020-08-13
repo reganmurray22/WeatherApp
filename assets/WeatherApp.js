b94132e40a22f7b085faa3d9403b8eb490414605
reganmurray22 <67241915+reganmurray22@users.noreply.github.com>
2020-08-13 05:54:35 -0500

    buttons, uv

--- assets/WeatherApp.js
+++ assets/WeatherApp.js
@@ -4,7 +4,10 @@ renderCityButtons();
 
 
 function getForecast() {            
-  
+
+var lat = "";
+var lon = "";    
+
 // Creates AJAX call for the specific movie button being clicked       
         $.ajax({
             url:"https://api.openweathermap.org/data/2.5/weather?q=" + selectCity + "&appid=33c63abdc074bfb89c02e1a51923b28a",
@@ -19,6 +22,9 @@ function getForecast() {
             const temperature = $("<p>");
             const humidity = $("<p>");
             const dateTime = (response.dt);
+            const lat = (response.coord.lat);
+            const lon = (response.coord.lon);
+            const uvIndex = $("<span>").attr("id", "uv");
 
 //convert unixtimestamp
 
@@ -38,18 +44,39 @@ function getForecast() {
 // Displays the chosen city
             cityName.text(response.name + "            " + convdateTime);
             weatherDescription.text("Currently: " + response.weather[0].main);
-            temperature.text("Current Temperature: "  + fahrenheitTemp + " F");                    
+            temperature.text("Current Temperature: "  + fahrenheitTemp + "F");                    
             humidity.text("Humidity: "  + response.main.humidity + "%");
 
+
+//second API call for UV           
+            $.ajax({
+                url:"https://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + "&appid=33c63abdc074bfb89c02e1a51923b28a",
+                method: "GET"
+            }).then(function (response) {
+
+                var index = (response.value);
+//changing color of uv index
+                  if (index>=0 && index<=2){
+                    $("#uv").text("UV Index: " + index).css("background-color", "green").attr("font-weight", "bold");
+                  } else if (index<=3 && index<=5) {
+                    $("#uv").text("UV Index: " + index).css("background-color", "yellow").attr("font-weight", "bold");
+                  } else if (index<=6 && index<=7) {
+                    $("#uv").text("UV Index: " + index).css("background-color", "orange").attr("font-weight", "bold");
+                  } else {
+                    $("#uv").text("UV Index: " + index).css("background-color", "red").attr("font-weight", "bold");
+                  }                   
+            }); 
+
 //All items can be chained in jQuery.
-            $("#chosenCity").prepend(cityName, weatherIcon, weatherDescription, temperature, humidity);
+            $("#chosenCity").prepend(cityName, weatherIcon, weatherDescription, temperature, humidity, uvIndex);
         });
+   
  
-//second API call for five day
+//third API call for five day
         $.ajax({
             url:"https://api.openweathermap.org/data/2.5/forecast?q=" + selectCity + "&appid=33c63abdc074bfb89c02e1a51923b28a",
             method: "GET"
-        }).then(function forecast(response) { 
+        }).then(function (response) { 
             
 //create initial empty array
             fiveDayArray = [];
@@ -57,15 +84,15 @@ function getForecast() {
 //loop to pull out the necessary data from the API call increasing index by 8
             for (var f = 1; f<=33; f += 8) { 
                 var day = (response.list[f].dt_txt.split(" ")[0]);
-                var weatherIcon = (response.list[f].weather[0].icon);
-                var forecast = ('Weather: ' + response.list[f].weather[0].description);
+                var weatherIcon = ("http://openweathermap.org/img/wn/" + response.list[f].weather[0].icon  + "@2x.png");
+                var forecast = ("Weather: " + response.list[f].weather[0].description);
                     var fahrenheitTemp = Math.floor((response.list[f].main.temp - 273.15) * 1.8 + 32); 
-                var temp = ('Temperature: ' + fahrenheitTemp);
-                var humid = ('Humidity: ' + response.list[f].main.humidity + "%");
- 
+                var temp = ("Temperature: " + fahrenheitTemp + "F");
+                var humid = ("Humidity: " + response.list[f].main.humidity + "%");
+                
 //creating an object representing the forecast for each day
-                let fiveDayObject = {day, forecast, temp, humid};
-
+                let fiveDayObject = {day, weatherIcon, forecast, temp, humid};
+console.log(fiveDayObject);
 //pushing all five objects into the initial array
                 fiveDayArray.push(fiveDayObject);
             }
@@ -74,19 +101,18 @@ function getForecast() {
             fiveDayArray.forEach(function (days) {
 
                 var cardBody = $("<div>").attr("class", "card text-white bg-primary mb-3",);
-                $("#fiveDayForecast").append(cardBody);
                 var thisDay = $("<div>").text(`${days.day}`);
-                var icon = (`${days.weatherIcon}`)
-                var thisIcon = $('<img />').attr("src", "http://openweathermap.org/img/wn/" + icon + "@2x.png");
+                var thisIcon = $('<img />').attr("src", `${days.weatherIcon}`)
                 var thisForecast = $("<div>").text(`${days.forecast}`);
                 var thisTemp = $("<div>").text(`${days.temp}`);
                 var thisHumid = $("<div>").text(`${days.humid}`);
-                $(".card").append(thisDay, thisIcon,thisForecast, thisTemp,thisHumid);
+                $("#fiveDayForecast").append(cardBody);
+                cardBody.append(thisDay, thisIcon,thisForecast, thisTemp, thisHumid);
              });
         });
 }
 
-function renderCityButtons() {
+function renderCityButtons(array) {
     $('#searchList').empty();
     for (var i = 0; i < citiesList.length; i++) {
         var x = $("<button>").addClass("city").attr("data-name", citiesList[i]).text(citiesList[i]);
@@ -98,16 +124,13 @@ $(document).ready(function() {
     $("#search").on("click", function(event){
         event.preventDefault();
         selectCity = $("#city-input").val().trim();
+        citiesList.push(selectCity);
+        localStorage.setItem("savedCitiesList", JSON.stringify(citiesList));
+        renderCityButtons();
         getForecast(selectCity);
+});
 
-//add city to saved cities
-        $("#add-city").on("click", function addCity(){
-            var newCity = $(selectCity).val();
-            citiesList.push(newCity);
-            localStorage.setItem("savedCitiesList", JSON.stringify(citiesList));
-        });
-
-    })
+    
 
 
 //add event listener to all city buttons
